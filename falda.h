b7d2f15a06b41878a18ce0cda0745b04e18e862e
PiliDavila17 <93629747+PiliDavila17@users.noreply.github.com>
2021-12-01 17:17:06 -0600

    Update falda.h

--- falda.h
+++ falda.h
@@ -1,15 +1,32 @@
+/*
+Autor: Maria del Pilar Davila Verduzco
+Matrícula: A01708943
+Fecha: Noviembre 2021
 
-//Clase faldas
-//Objetos 
-//	talla (CH,M,G,XG)
-//	Tipo (corta, larga, midi)
+Clase faldas hija de Ropa
+Objetos:
+	talla (CH,M,G,XG)
+	Tipo (corta, larga, midi)
+*/
+
+
+#ifndef FALDAS_H_
+#define FALDAS_H_
 
 #include "ropa.h"
 
+#include <sstream>
+#include <iostream>
 
-class Faldas: public Ropa{
-	
-	public:
-		Faldas(string ta, string ti): Ropa(ta,ti){};
+using namespace std;
+
+//declaracion de la clase
+class Falda : public Ropa {
+
+public://constructor del objeto
+	Falda() {};
+	Falda(string ta, string ti) : Ropa(ta, ti) {};
 
 };
+
+#endif
