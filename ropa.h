ef7dd8af5e9699bbff4161fe792fb231336757ba
PiliDavila17 <93629747+PiliDavila17@users.noreply.github.com>
2021-12-01 17:18:39 -0600

    Update ropa.h

--- ropa.h
+++ ropa.h
@@ -1,42 +1,47 @@
-//Clase Ropa
-//Super Clase de la Tienda de Ropa
-//Objetos 
-//	talla (CH,M,G,XG)
-//	Tipo (Mezclilla o Vestir)
+/*
+Autor: Maria del Pilar Davila Verduzco
+Matrícula: A01708943
+Fecha: Noviembre 2021
+
+Clase Ropa
+Super Clase del Proyecto "Tienda de Ropa", la cual tiene tres clases hijas
+Blusa, Pantalon y Falda
+*/
 
 #ifndef ROPA_H
 #define ROPA_H
 
 #include<iostream>
 
-
 using namespace std;
+//Declaracion de la clase
+class Ropa {
+//Variables que tendrán todas las clases hijas
+protected:
+	string talla;
+	string tipo;
+//Metodos que tendrá el objeto
+public:
+	Ropa() {}; //constructor
+	Ropa(string ta, string ti) {
+		talla = ta;
+		tipo = ti;
+	}
+	string getTalla() {
+		return talla;
+	}
 
-class Ropa{
-	private:
-		string talla;
-		string tipo;
-	
-	public:
-		Ropa(string ta, string ti){
-			 talla=ta;
-			 tipo=ti;
-		}
-		string getTalla(){
-			return talla;
-		}
-		
-		string getTipo(){
-			return tipo;
-		}
-		
-		void setTalla(string ta){
-			talla=ta;
-		}
-		
-		void setTipo(string ti){
-			tipo=ti;
-		}
-		
+	string getTipo() {
+		return tipo;
+	}
+
+	void setTalla(string ta) {
+		talla = ta;
+	}
+
+	void setTipo(string ti) {
+		tipo = ti;
+	}
+
 };
 #endif
