20bc8a5aa5237d054f6698b1e22c36e2b0e55630
PiliDavila17 <93629747+PiliDavila17@users.noreply.github.com>
2021-12-01 17:16:36 -0600

    Update blusa.h

--- blusa.h
+++ blusa.h
@@ -1,15 +1,30 @@
+/*
+Autor: Maria del Pilar Davila Verduzco
+Matrícula: A01708943
+Fecha: Noviembre 2021
 
-//Clase Blusas
-//Objetos 
-//	talla (CH,M,G,XG)
-//	Tipo (manga larga o manga corta)
+Clase Blusas
+Objetos
+talla (CH,M,G,XG)
+	Tipo (manga larga o manga corta)
+*/
 
+#ifndef BLUSAS_H_
+#define BLUSAS_H_
+
+#include <iostream>
 #include "ropa.h"
 
+using namespace std;
 
-class Blusas: public Ropa{
-	
-	public:
-		Blusas(string ta, string ti): Ropa(ta,ti){};
+
+//Declaracion de la clase
+class Blusa : public Ropa {
+
+public:
+	Blusa() {};//Constructor
+	Blusa(string ta, string ti) : Ropa(ta, ti) {};
 
 };
+
+#endif
