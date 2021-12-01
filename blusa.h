334c1a1c5f4be763374168b203395159f120a6cc
PiliDavila17 <93629747+PiliDavila17@users.noreply.github.com>
2021-12-01 17:06:12 -0600

    Add files via upload

--- blusa.h
+++ blusa.h
@@ -1,30 +1,15 @@
-/*
-Autor: Maria del Pilar Davila Verduzco
-Matrícula: A01708943
-Fecha: Noviembre 2021
 
-Clase Blusas
-Objetos
-talla (CH,M,G,XG)
-	Tipo (manga larga o manga corta)
-*/
+//Clase Blusas
+//Objetos 
+//	talla (CH,M,G,XG)
+//	Tipo (manga larga o manga corta)
 
-#ifndef BLUSAS_H_
-#define BLUSAS_H_
-
-#include <iostream>
 #include "ropa.h"
 
-using namespace std;
 
-
-
-class Blusa : public Ropa {
-
-public:
-	Blusa() {};
-	Blusa(string ta, string ti) : Ropa(ta, ti) {};
+class Blusas: public Ropa{
+	
+	public:
+		Blusas(string ta, string ti): Ropa(ta,ti){};
 
 };
-
-#endif
--- falda.h
+++ falda.h
@@ -1,32 +1,15 @@
-/*
-Autor: Maria del Pilar Davila Verduzco
-Matrícula: A01708943
-Fecha: Noviembre 2021
 
 //Clase faldas
-//Objetos
+//Objetos 
 //	talla (CH,M,G,XG)
 //	Tipo (corta, larga, midi)
-*/
-
-
-#ifndef FALDAS_H_
-#define FALDAS_H_
 
 #include "ropa.h"
 
-#include <sstream>
-#include <iostream>
 
-using namespace std;
-
-
-class Falda : public Ropa {
-
-public:
-	Falda() {};
-	Falda(string ta, string ti) : Ropa(ta, ti) {};
+class Faldas: public Ropa{
+	
+	public:
+		Faldas(string ta, string ti): Ropa(ta,ti){};
 
 };
-
-#endif#pragma once
