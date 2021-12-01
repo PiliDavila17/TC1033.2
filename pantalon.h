054b2448e3fdea02d6ccd76e8d2e5752a434a1be
PiliDavila17 <93629747+PiliDavila17@users.noreply.github.com>
2021-12-01 17:18:05 -0600

    Update pantalon.h

--- pantalon.h
+++ pantalon.h
@@ -1,23 +1,42 @@
-//Clase Pantalones
-//Hija de Ropa
+/*
+Autor: Maria del Pilar Davila Verduzco
+Matrícula: A01708943
+Fecha: Noviembre 2021
 
+Clase Pantalones
+Hija de Ropa
+*/
+
+
+#ifndef PANTALONES_H_
+#define PANTALONES_H_
 #include"ropa.h"
 
+#include <sstream>
+#include <iostream>
 
-class Pantalones: public Ropa{
-	
-	private: 
-		string epoca;
-	
-	public:
-		Pantalones(string ta, string ti, string ep):Ropa(ta, ti){
-		epoca=ep;
-		}
-		string getEpoca(){
-			return epoca;
-		}
+using namespace std;
 
-void setEpoca(string ep){
-	epoca=ep;
-}
+//Declaración de la clase
+class Pantalon : public Ropa {
+//Variable que tendrá únicamente esta clase
+private:
+	string epoca;
+//Declaracion de métodos y constructores
+public:
+	Pantalon() {};
+	Pantalon(string ta, string ti, string ep) :Ropa(ta, ti) {
+		epoca = ep;
+	}
+	string getEpoca() {
+		return epoca;
+	}
+	void setEpoca(string ep);
+	string to_string();
 };
+
+void Pantalon::setEpoca(string ep) {
+	epoca = ep;
+};
+
+#endif
